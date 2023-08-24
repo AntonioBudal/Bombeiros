1301e9402621a819b1c5566d364c3199940e02c3
antonio_b_lim <you@example.com>
2023-08-24 11:22:30 -0300

    Bombeiros

---
+++ pagina_principal.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="pagina_principal.css">
+</head>
+<body>
+        <?php 
+            include("menu.html");
+        ?>
+    <div class="pagina">
+        <?php 
+            include("tipo_ocorrencia.html");
+        ?>
+    </div>
+    <div class="pagina ">
+        <?php 
+            include("localizacao_traumas.php");
+        ?>
+    </div>
+    <div class="pagina ">
+        <?php 
+            include("dados_paciente.html");
+        ?>
+    </div>
+    <div class="pagina ">
+        <?php 
+            include("observacoes.html");
+        ?>
+    </div>
+    <div class="pagina">
+        <?php 
+            include("nivel_consciencia.html");
+        ?>
+    </div>
+    <div class="pagina"></div>
+    
+ 
+</body>
+</html>
